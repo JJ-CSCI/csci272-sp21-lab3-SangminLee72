092d34146ab3232123123a64c23437c886060453
SangminLee72 <ysmin721@gmail.com>
2021-03-02 20:26:39 +0000

    I wrote a code

--- main.cpp
+++ main.cpp
@@ -7,6 +7,73 @@
 // Put the assignment code here
 
 class Quaternion {
+  public:
+    Quaternion(){
+      coefficient1_ = 0.0;
+      coefficient2_ = 0.0;
+      coefficient3_ = 0.0;
+      coefficient4_ = 0.0;
+    };
+
+    Quaternion(double coefficient1, double coefficient2, double coefficient3, double coefficient4){
+      coefficient1_ = coefficient1;
+      coefficient2_ = coefficient2;
+      coefficient3_ = coefficient3;
+      coefficient4_ = coefficient4;
+    };
+    
+    double getCoefficient1() const {
+      return coefficient1_;
+    }
+
+    double getCoefficient2() const {
+      return coefficient2_;
+    }
+
+    double getCoefficient3() const {
+      return coefficient3_;
+    }
+
+    double getCoefficient4() const {
+      return coefficient4_;
+    }
+
+    bool operator== (const Quaternion& rhs) const { 
+      return coefficient1_ == rhs.getCoefficient1() && 
+             coefficient2_ == rhs.getCoefficient2() &&
+             coefficient3_ == rhs.getCoefficient3() &&
+             coefficient4_ == rhs.getCoefficient4();
+    }
+
+    Quaternion operator+ (const Quaternion& rhs) const {
+      Quaternion q(coefficient1_ + rhs.getCoefficient1(), 
+                   coefficient2_ + rhs.getCoefficient2(), 
+                   coefficient3_ + rhs.getCoefficient3(), 
+                   coefficient4_ + rhs.getCoefficient4());
+      return q;
+    }
+
+    Quaternion operator- (const Quaternion& rhs) const {
+      Quaternion p(coefficient1_ - rhs.getCoefficient1(), 
+                   coefficient2_ - rhs.getCoefficient2(), 
+                   coefficient3_ - rhs.getCoefficient3(), 
+                   coefficient4_ - rhs.getCoefficient4());
+      return p;
+    }
+
+    Quaternion operator* (const double& y) const {
+      Quaternion t(coefficient1_ * y, 
+                   coefficient2_ * y, 
+                   coefficient3_ * y, 
+                   coefficient4_ * y);
+      return t;
+    }
+    
+  private:
+    double coefficient1_;
+    double coefficient2_; 
+    double coefficient3_;
+    double coefficient4_;
 };
 
 //------------------------------
